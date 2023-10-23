c1e41169e55be21d81e2d7f5877494c07410509b
rakibulislam8226 <rakibulislam8226@gmail.com>
2023-10-23 19:00:44 +0600

    Added Pipelinies for sqlite3

--- src/scrapyproject/pipelines.py
+++ src/scrapyproject/pipelines.py
@@ -5,9 +5,39 @@
 
 
 # useful for handling different item types with a single interface
-from itemadapter import ItemAdapter
+import sqlite3
 
 
 class ScrapyprojectPipeline:
+    def __init__(self) -> None:
+        self.create_connection()
+        self.create_table()
+
+    def create_connection(self, item):
+        self.conn = sqlite3.connect("db.sqlite3")
+        self.curr = self.conn.cursor()
+
+    def create_table(self, item):
+        self.curr.execute("""DROP TABLE IF EXISTS quotes_db""")
+        self.curr.execute(
+            """create table quotes_db (
+                    title text,
+                    author text,
+                    tag text
+        )"""
+        )
+
     def process_item(self, item, spider):
+        print("=======================================================")
+        self.store_db(item)
         return item
+
+    def store_db(self, item):
+        self.curr.execute(
+            """insert into quotes_db values (?,?,?)"""(
+                item["title"][0],
+                item["author"][0],
+                item["tag"],
+            )
+        )
+        self.conn.commit()
